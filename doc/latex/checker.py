0042f0b41cd1553d6e3ad120d3c38493cf28057b
Patryk Bęza <patryk.beza@gmail.com>
2017-03-17 00:46:59 +0100

    Dodanie rozdziału o protokole do multicastu PGM i początki opisu architektury projektowanego protokołu sieciowego

--- doc/latex/checker.py
+++ doc/latex/checker.py
@@ -5,19 +5,23 @@ import re
 
 mistakes_found = False
 
-def print_line_matches(line, line_no, fpath, regex):
+def print_line_matches(line, line_no, fpath, hardspaces_regex, labels_regex):
 	global mistakes_found
-	match = regex.search(line)
+	whitespaces_match = hardspaces_regex.search(line)
+	labels_match = labels_regex.search(line)
 
-	if match:
+	if whitespaces_match or labels_match:
 		if mistakes_found == False:
 			print("--------------------------------------------------------\n")
 			print("MISTAKES IN FILE '" + fpath + "':\n")
 		mistakes_found = True
-		print("      Match '" + str(match.group(0)) + "' in line " + str(line_no) + ":")
+		if whitespaces_match:
+			print("      Match '" + str(whitespaces_match.group(0)) + "' in line " + str(line_no) + ":")
+		if labels_match:
+			print("      Match '" + str(labels_match.group(0)) + "' in line " + str(line_no) + ":")
 		print("      Line: '" + line.strip() + "'\n")
 
-def print_latex_mistakes(fpath, regex):
+def print_latex_mistakes(fpath, hardspaces_regex, labels_regex):
 	global mistakes_found
 	line_no = 0
 	mistakes_found = False
@@ -27,26 +31,36 @@ def print_latex_mistakes(fpath, regex):
 			line_no = line_no + 1
 			if line.strip().startswith("%"):
 				continue
-			print_line_matches(line, line_no, fpath, regex)
+			print_line_matches(line, line_no, fpath, hardspaces_regex, labels_regex)
 
 def add_regex_on_sides(s):
+#	xstr = '[^\w~]'
+	xstr = '\s'
 	return xstr + s + xstr
+
+def get_hardspaces_regex():
+	keywords = [ 'a', 'ci', 'co', 'do', 'gdy', 'i', 'lub', 'm.in.', 'nad', 'np.', 'o', 'oraz', 'pod', 'przy', 'u', 'ta', 'to', 'tzn.', 'w', 'z', 'za', 'ze', 'że' ]
+	keywords_extended = [ add_regex_on_sides(s) for s in keywords ]
+	wregex = '(' + '|'.join(keywords_extended) + ')'
+	print("Whitespaces regex for all the '*" + file_extension + "' files:")
+	print("'" + wregex + "'\n")
+	return re.compile(wregex)
+
+def get_unqualified_labels_regex():
+	lregex = r'.*((\\label|\\ref){(?!.*(ch:|sec:|subsec:|fig:|tab:|eq:|lst:|itm:|alg:|app:)).*}.*)'
+	print("Labels regex for all the '*" + file_extension + "' files:")
+	print("'" + lregex + "'\n")
+	return re.compile(lregex)
 
 if __name__ == '__main__':
 	rootdir = '.'
 	file_extension = '.tex'
-	keywords = [ 'a', 'ci', 'co', 'do', 'gdy', 'i', 'lub', 'm.in.', 'nad', 'np.', 'o', 'oraz', 'pod', 'przy', 'u', 'ta', 'to', 'tzn.', 'w', 'z', 'za', 'ze', 'że' ]
-#	xstr = '[^\w~]'
-	xstr = '\s'
-	keywords_extended = [ add_regex_on_sides(s) for s in keywords ]
-	regex = '(' + '|'.join(keywords_extended) + ')'
 
-	print("Regex for all the '*" + file_extension + "' files: '" + regex + "'\n")
-
-	regex = re.compile(regex)
+	hardspaces_regex = get_hardspaces_regex()
+	labels_regex = get_unqualified_labels_regex()
 
 	for root, _, files in os.walk(rootdir):
 		for f in files:
 			if f.endswith(file_extension):
 				fpath = os.path.join(root, f)
-				print_latex_mistakes(fpath, regex)
+				print_latex_mistakes(fpath, hardspaces_regex, labels_regex)
